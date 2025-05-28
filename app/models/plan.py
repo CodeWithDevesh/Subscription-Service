ae4ba6dcb7b392778891b5d3792b3f4bef1fa48b
Devesh <deveshagarwal652005@gmail.com>
2025-05-28 15:48:22 +0530

    database models setted up

---
+++ app/models/plan.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+class Plan(Base):
+    __tablename__ = "plans"
+
+    id = Column(Integer, primary_key=True, index=True) 
+    name = Column(String, nullable=False, unique=True, index=True)
+    price = Column(Integer, nullable=False)
+    duration_days = Column(Integer, nullable=False)
+    features = Column(JSONB)
+    # subscribers = relationship("Subscription", back_populates="plan")
---
+++ app/models/subscription.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime, timezone
+from app.database import Base
+from sqlalchemy import Enum as SqlEnum
+import enum
+
+class SubscriptionStatus(enum.Enum):
+    ACTIVE = "ACTIVE"
+    INACTIVE = "INACTIVE"
+    CANCELLED = "CANCELLED"
+    EXPIRED = "EXPIRED"
+
+class Subscription(Base):
+    __tablename__ = "subscriptions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False, unique=True)
+    plan_id = Column(Integer, ForeignKey("plans.id"), nullable=False)
+    start_date = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+    end_date = Column(DateTime, nullable=True)
+    status = Column(SqlEnum(SubscriptionStatus), default=SubscriptionStatus.ACTIVE, nullable=False)
+
+    user = relationship("User", back_populates="subscriptions")
+    plan = relationship("Plan", back_populates="subscriptions")
